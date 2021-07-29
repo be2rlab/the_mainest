733bba4539f40f46107b2a676168eae673a01a50
kirillin <kaartemov@gmail.com>
2021-07-30 00:46:51 +0300

    Add all states without PnP

--- src/the_mainest_node.py
+++ src/the_mainest_node.py
@@ -3,6 +3,7 @@
 from typing import Mapping
 import rospy
 import numpy as np
+from rospy.core import loginfo
 
 from std_msgs.msg import String
 from std_msgs.msg import Float32MultiArray
@@ -109,7 +110,7 @@ class TaskInterface(object):
         rospy.wait_for_service('give')
         try:
             srv_obj = rospy.ServiceProxy('give', Give)
-            resp = srv_obj(String(obj))
+            srv_obj(String(f"give {obj}"))
         except rospy.ServiceException as e:
             print(f"Service call failed: {e}")
 
@@ -133,6 +134,11 @@ class TaskInterface(object):
         status = status1 and status2
         return status
 
+def wait_for(sec):
+    rospy.loginfo(f"Wait [{sec}] sec")
+    for ti in range(sec):
+        rospy.loginfo(f"{ti+1} [sec]")
+        rospy.sleep(1)
 
 def main():
    
@@ -149,31 +155,59 @@ def main():
     state = 'idle'
     state_id = 0
     resp = True
+    obj_name = ''
+    data12numbers = []
 
     rate = rospy.Rate(30)
     while not rospy.is_shutdown():
 
-        print(state)
+        rospy.loginfo(f"Now: {state}")
 
-        if state == 'mapping' and resp:
+        if state == 'idle' and resp:
+            "IDLE STATE"
             resp = False
-            # resp = task_interface.mapping()
-            resp = True
+            key = input('Press any key to start or `q` to exit\n')
+            if key == 'q':
+                break
+            state, resp = 'candle', True
+        elif state == 'candle' and resp:
+            "CANDLE STATE"
+            resp = False
+            # resp = task_interface.go_to_js(np.array(CANDLE_POSE))
+            wait_for(1)
+            state, resp = 'initial', True
         elif state == 'initial' and resp:
+            "INITIAL STATE"
             resp = False
-            resp = task_interface.initial()
+            resp = True # task_interface.go_to_js(np.array(INITIAL_POSE))
+            state = 'give'
+        elif state == 'give' and resp:
+            "GIVE STATE"
+            resp = False
+            if obj_name == '':
+                obj_name = input('Look at the `/segmented_view` topic and choose the the object name to grasp:\n')
+            print(f"Choosed object is: {obj_name}")
+            task_interface.call_give(obj_name)
+            wait_for(1)
+            state, resp = 'get_bbox', True
+        elif state == 'get_bbox' and resp:
+            "GET BBOX STATE"
+            resp = False
+            data12numbers = task_interface.call_obb_arr_srv()
+            print("DATA 12 numbers: ", data12numbers.data)
+            if len(data12numbers.data) == 12:
+                obj_name = ''
+                state, resp = 'pnp', True
+            else:
+                rospy.logwarn("Data from `add_object` is not recived")
+                rospy.logwarn("Trying to get one more times...")
+                data12numbers = []
+                state, resp = 'give', True
         elif state == 'pnp' and resp:
+            "PnP STATE"
             resp = False
-            resp = task_interface.pnp()
-        else:
-            resp = task_interface.go_to_js(np.array(CANDLE_POSE)* radians)
-            rospy.loginfo("Return to initial CANDLE pose")
-        
-        if resp == True:
-            state = TASK[state_id]
-            state_id = state_id + 1
-            if state_id > 3:
-                state_id = 0
+            
+            state = 'idle'
 
         rate.sleep()
 
