e23f897f5b7862172983b90a1e6664e2f94ebbdf
kirillin <kaartemov@gmail.com>
2021-07-30 01:49:24 +0300

    Add all states without testing

--- src/the_mainest_node.py
+++ src/the_mainest_node.py
@@ -7,11 +7,12 @@ from rospy.core import loginfo
 
 from std_msgs.msg import String
 from std_msgs.msg import Float32MultiArray
+from geometry_msgs.msg import Pose, Point, Quaternion
 
 from msdp.srv import GoalPoses, JSPosition, JSPositionResponse, CartPosition, CartPositionResponse
 from uhvat_ros_driver.srv import SetGripperState
 
-from the_mainest.srv import Give, ObbArr
+from the_mainest.srv import Give, ObbArr, GetPNPPoses, AttachObj
 
 
 radians = np.pi / 180
@@ -34,29 +35,118 @@ TASK = [
     'pnp'
 ]
 
+# TODO set the points
+BOXES = {
+    'box': Pose(Point(), Quaternion()),
+    'mouse': Pose(Point(), Quaternion()),
+    'regbi': Pose(Point(), Quaternion()),
+    'cup': Pose(Point(), Quaternion())
+}
+
+
+def wait_for(sec):
+    rospy.loginfo(f"Wait [{sec}] sec")
+    for ti in range(sec):
+        rospy.loginfo(f"{ti+1} [sec]")
+        rospy.sleep(1)
 
 class TaskInterface(object):
 
     def __init__(self) -> None:
         super().__init__()
-        pass
+        
+        self.pnp_state = 'idl'
+        # [
+        #     'idl',
+        #     'p1', 
+        #     'p1*', 
+        #     'gripper_close', 
+        #     'attach_obj' 
+        #     'p2', 
+        #     'p1*', 
+        #     'gripper_open', 
+        #     'dettach_obj'
+        # ]
 
-    def pnp(self):
-        resp = False
-        cnt = 0
-        while cnt < 10:
-            print(cnt)
-            if cnt % 2 == 0:
-                resp = self.go_to_js(np.array(CANDLE_POSE) * radians)
+    def pnp(self, obj_name, grasp_pose):
+        resp = True
+
+        while resp:
+            rospy.loginfo(f"PnP STATE: {self.pnp_state}")
+
+            if self.pnp_state == 'idl' and resp:
+                resp = False
+                wait_for(5)
+                state, resp = 'p1*', True
+
+            elif self.pnp_state == 'p1*' and resp:
+                # p1* STATE -- over object
+                resp = False
+
+                p1 = grasp_pose
+                p1.position.z = p1.position.z + 0.5
+                grasp_pose_safe = CartPosition(p1)
+
+                r = self.go_to_cart(grasp_pose_safe)
+                resp = r.status
+                state = 'p1'
+
+            elif self.pnp_state == 'p1' and resp:
+                resp = False
+
+                p1 = grasp_pose
+                p1.position.z = p1.position.z + 0.3
+                grasp_pose = CartPosition(p1)
+
+                r = self.go_to_cart(grasp_pose)
+                resp = r.status
+                state = 'gripper_close'
+
+            elif self.pnp_state == 'gripper_close' and resp:
+                resp = False
+                self.gripper(6)
+                wait_for(2)
+                state, resp = 'attach_obj', True
+
+            elif self.pnp_state == 'attach_obj' and resp:
+                resp = False
+                resp = self.call_attach_obj('obj', True)
+                state = 'p2*'
+
+            elif self.pnp_state == 'p2*' and resp:
+                resp = False
+
+                p2 = BOXES[obj_name]
+                p2.position.z = p2.position.z + 0.5
+                ungrasp_pose = CartPosition(p2)
+
+                r = self.go_to_cart(ungrasp_pose)
+                resp = r.status
+                state = 'p2'
+
+            elif self.pnp_state == 'p2' and resp:
+                resp = False
+
+                p2 = BOXES[obj_name]
+                p2.position.z = p2.position.z + 0.3
+                ungrasp_pose = CartPosition(p2)
+
+                r = self.go_to_cart(ungrasp_pose)
+                resp = r.status
+                state = 'gripper_open'
+
+            elif self.pnp_state == 'gripper_open' and resp:
+                resp = False
                 self.gripper(0)
-            else:
-                resp = self.go_to_js(np.array(CANDLE_POSE) * radians - 0.1)
-                self.gripper(5)
-            if not resp:
-                rospy.logwarn(f"Mapping pose {i} do not feasible")
-            cnt = cnt + 1
+                wait_for(2)
+                state, resp = 'dettach_obj', True
 
-        return resp
+            elif self.pnp_state == 'dettach_obj' and resp:
+                resp = False
+                resp = self.call_attach_obj('obj', False)
+                state = 'exit'
+            elif self.pnp_state == 'exit' and resp:
+                return True
 
     def initial(self):
         return self.go_to_js(np.array(CANDLE_POSE) * radians)
@@ -124,6 +214,29 @@ class TaskInterface(object):
         except rospy.ServiceException as e:
             print(f"Service call failed: {e}")
 
+    def call_get_pnp_poses(self, data):
+        """ 
+        Returns only p1 !!! (and can return p2 
+        (p2 hardcoded for output box position, but not used here) 
+        """
+        rospy.wait_for_service('get_pnp_poses')
+        try:
+            srv_obj = rospy.ServiceProxy('get_pnp_poses', GetPNPPoses)
+            resp = srv_obj(Float32MultiArray(data))
+            return resp.p1
+        except rospy.ServiceException as e:
+            print(f"Service call failed: {e}")
+
+    def call_attach_obj(self, obj_id, gripper_state):
+        rospy.wait_for_service('attach_obj')
+        try:
+            srv_obj = rospy.ServiceProxy('attach_obj', AttachObj)
+            resp = srv_obj(obj_id, gripper_state)
+            return resp.status
+        except rospy.ServiceException as e:
+            print(f"Service call failed: {e}")
+
+
     def call_pnp(self, p1, p2):
         status = False
 
@@ -134,11 +247,6 @@ class TaskInterface(object):
         status = status1 and status2
         return status
 
-def wait_for(sec):
-    rospy.loginfo(f"Wait [{sec}] sec")
-    for ti in range(sec):
-        rospy.loginfo(f"{ti+1} [sec]")
-        rospy.sleep(1)
 
 def main():
    
@@ -157,6 +265,7 @@ def main():
     resp = True
     obj_name = ''
     data12numbers = []
+    cnt_try_give = 0
 
     rate = rospy.Rate(30)
     while not rospy.is_shutdown():
@@ -184,29 +293,55 @@ def main():
         elif state == 'give' and resp:
             "GIVE STATE"
             resp = False
-            if obj_name == '':
+            
+            if cnt_try_give > 5:    # check if try to give 5 times 
+                cnt_try_give = 0
+                obj_name = ''    
+            
+            if obj_name == '':      # ask to enter the object name
                 obj_name = input('Look at the `/segmented_view` topic and choose the the object name to grasp:\n')
+
             print(f"Choosed object is: {obj_name}")
             task_interface.call_give(obj_name)
             wait_for(1)
             state, resp = 'get_bbox', True
+
         elif state == 'get_bbox' and resp:
             "GET BBOX STATE"
             resp = False
             data12numbers = task_interface.call_obb_arr_srv()
             print("DATA 12 numbers: ", data12numbers.data)
             if len(data12numbers.data) == 12:
-                obj_name = ''
-                state, resp = 'pnp', True
+                state, resp = 'get_pnp_poses', True
             else:
                 rospy.logwarn("Data from `add_object` is not recived")
                 rospy.logwarn("Trying to get one more times...")
                 data12numbers = []
                 state, resp = 'give', True
+                cnt_try_give = cnt_try_give + 1
+        
+        elif state == 'get_pnp_poses' and resp:
+            "GET PnP POSES STATE"
+            resp = False
+            p1 = task_interface.call_get_pnp_poses(data12numbers)
+            
+            if type(p1) is Pose:   # check for Pose data validity
+                if type(p1.position.x) is float:
+                    state, resp = 'pnp', True
+                else:
+                    rospy.logwarn("PnP STATE: the grasp pose is bad")
+            else:
+                rospy.logwarn("call the service `get_pnp_poses` one more times...")
+                resp = True
+
         elif state == 'pnp' and resp:
             "PnP STATE"
             resp = False
+            resp = task_interface.pnp(obj_name, p1)
             
+            p1 = Pose()
+            data12numbers = []
+            obj_name = ''
             state = 'idle'
 
         rate.sleep()
